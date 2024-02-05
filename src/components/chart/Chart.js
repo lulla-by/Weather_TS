e75b4ab0e3053938e2f8f7309d8eb785007704dc
lulla-by <thfl3363@hanmail.net>
2024-02-05 18:53:09 +0900

    Chart 컴포넌트 수정 - 막대, 꺾은선 그래프로 수정 - 데이터 라벨 및 기호 추가 - 강수량 및 온도 세로축 최댓값, 최솟값 설정

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,81 +1,132 @@
 import React, { useState } from 'react';
-import ApexCharts from 'react-apexcharts'
-import {useSelector } from 'react-redux';
+import ApexCharts from 'react-apexcharts';
+import { useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import Loading from '../../ui/Loading';
 
 const Chart = () => {
-  const weatherData = useSelector(state=>state.chartWeather)
-  const isLoading = useSelector(state=>state.isLoading)
- const [chartShowStata,setChartShowData] = useState(true)
-const chartName = (chartShowStata?"Close Chart":"Show Chart")
+  const weatherData = useSelector((state) => state.chartWeather);
+  const isLoading = useSelector((state) => state.isLoading);
+  const [chartShowStata, setChartShowData] = useState(true);
+  const chartName = chartShowStata ? 'Close Chart' : 'Show Chart';
 
+  function convertDateTime(dateTime) {
+    const year = dateTime.slice(2, 4);
+    const month = dateTime.slice(4, 6);
+    const day = dateTime.slice(6, 8);
+    const hour = dateTime.slice(8, 10);
 
- function convertDateTime(dateTime) {
-  const year = dateTime.slice(2, 4);
-  const month = dateTime.slice(4, 6);
-  const day = dateTime.slice(6, 8);
-  const hour = dateTime.slice(8, 10);
-
-  return `${year}.${month}.${day} ${hour}시`;
-}
-
-
-  const filteredData = Object.entries(weatherData).map(([key, value]) => {
-    return value
-  });
-
-  const makeSeries = (data)=>{
-    let arr = [{name: 'Actual',
-    data:[
-    ]}]
-    for(let i = 0 ; i< data.length ; i++){
-      for(let key in data[i]){
-        let temperature = data[i][key].T1H
-        let precipitation = data[i][key].RN1 === "강수없음" ? 0 : parseFloat(data[i][key].RN1.replace('mm', ''));
-        let now = convertDateTime(key)
-        let obj = {
-          x:now,
-          y:temperature,
-          goals:[
-            {name:"강수량",
-            value :precipitation,
-            strokeHeight: 10,
-            strokeColor: '#4dabf7'
-          }
-          ]
-        }
-        arr[0].data.push(obj);
-      }
-    }
-    return arr
-
+    return `${year}.${month}.${day} ${hour}시`;
   }
 
-  const makedSeries = makeSeries(filteredData)
-  
-  const options = {
-    chart: {
-      height: 350,
-      type: 'bar'
-    },
-    colors: ['#fcc419'],
-    plotOptions: {
-      bar: {
-        columnWidth: '45%'
+  const filteredData = Object.entries(weatherData).map(([key, value]) => {
+    return value;
+  });
+
+  console.log(filteredData);
+
+  const makeSeries = (data) => {
+    let temperatureData = [];
+    let precipitationData = [];
+    let nowArr = [];
+
+    for (let i = 0; i < data.length; i++) {
+      for (let key in data[i]) {
+        let temperature = data[i][key].T1H;
+        let precipitation =
+          data[i][key].RN1 === '강수없음'
+            ? 0
+            : parseFloat(data[i][key].RN1.replace('mm', ''));
+        let now = convertDateTime(key);
+        nowArr.push(now);
+        temperatureData.push(temperature);
+
+        precipitationData.push(precipitation);
       }
-    },
+    }
+
+    return [
+      {
+        name: '강수량',
+        data: precipitationData,
+        type: 'column', // 막대 차트로 설정
+      },
+      {
+        name: '기온',
+        data: temperatureData,
+        type: 'line', // 꺾은선 차트로 설정
+        nowArr,
+      },
+    ];
   };
 
+  const makedSeries = makeSeries(filteredData);
 
-  if((isLoading === true || weatherData=== "") && chartShowStata === true) {
-    return <Loading/>
+  const minMax = (array) => {
+    return { max: Math.max(...array), min: Math.min(...array) };
+  };
+
+  const precipitationData = minMax(makedSeries[0].data);
+  const temperatureData = minMax(makedSeries[1].data);
+
+  let options = {
+    series: makedSeries,
+    chart: {
+      height: 350,
+      type: 'line',
+    },
+    stroke: {
+      width: [0, 4],
+      colors: ['#fcc419'],
+    },
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: [0, 1],
+      formatter: function (val, { seriesIndex }) {
+        if (seriesIndex === 0) {
+          return val.toFixed(2) + 'mm';
+        } else if (seriesIndex === 1) {
+          return val.toFixed(1) + '℃';
+        }
+        return val;
+      },
+    },
+    labels: makedSeries[1].nowArr,
+    yaxis: [
+      {
+        title: {
+          text: '강수량',
+        },
+        max: precipitationData.max + 5, // 강수량 세로축 최댓값
+        min: 0,
+      },
+      {
+        opposite: true,
+        title: {
+          text: '온도',
+        },
+        max: temperatureData.max + 5, // 온도 세로축 최댓값
+        min: temperatureData.min - 5,
+      },
+    ],
+  };
+
+  if ((isLoading === true || weatherData === '') && chartShowStata === true) {
+    return <Loading />;
   }
 
   return (
     <div>
-      {chartShowStata && <ApexCharts options={options} series={makedSeries} type="bar" height={350} />}
-      <Button onClick={()=>{setChartShowData(!chartShowStata)}} >{chartName}</Button>
+      {chartShowStata && (
+        <ApexCharts options={options} series={makedSeries} height={350} />
+      )}
+      <Button
+        onClick={() => {
+          setChartShowData(!chartShowStata);
+        }}
+      >
+        {chartName}
+      </Button>
     </div>
   );
 };
