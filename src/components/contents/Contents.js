f8d4ef8a7c6f2fafdde80500df8e14cce336d359
lulla-by <thfl3363@hanmail.net>
2024-02-07 22:31:39 +0900

    Contents 컴포넌트 리팩토링 - 가독성 고려하여 삼항 연산자에서 switch로 변경

--- src/components/contents/Contents.js
+++ src/components/contents/Contents.js
@@ -1,47 +1,85 @@
-import React from 'react'
+import React from 'react';
 import Information from './Information';
 import Clothes from './Clothes';
-import {useSelector } from 'react-redux';
-import classes from "./Contents.module.css"
+import { useSelector } from 'react-redux';
+import classes from './Contents.module.css';
 import Loading from '../../ui/Loading';
 
 const Contents = () => {
-  
-  const weatherData = useSelector(state=>state.chartWeather)
-  const isLoading = useSelector(state=>state.isLoading)
+  const weatherData = useSelector((state) => state.chartWeather);
+  const isLoading = useSelector((state) => state.isLoading);
   const filteredData = Object.entries(weatherData).map(([key, value]) => {
-    return value[key]
+    return value[key];
   });
-  const data = filteredData[0]
-  
+  const data = filteredData[0];
+
   let temperature, precipitation, humidity, PTY, SKY;
-  
+
   // temperature:기온, 강수량:precipitation, 습도:humidity
   if (data) {
     ({ T1H: temperature, RN1: precipitation, REH: humidity, PTY, SKY } = data);
   }
 
+  //강수형태 - 없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4)
+  let precipitationType;
 
+  switch (PTY) {
+    case "0":
+      console.log("없음");
+      precipitationType = '없음';
+      break;
+    case "1":
+      precipitationType = '비';
+      break;
+    case "2":
+      precipitationType = '비/눈';
+      break;
+    case "3":
+      precipitationType = '눈';
+      break;
+    case "4":
+      precipitationType = '소나기';
+      break;
+    default:
+      precipitationType = '없음';
+  }
 
-  //강수형태 - 없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4) 
-  const precipitationType = (PTY == 0 ? "없음" : (PTY == 1 ? "비" : (PTY == 2 ? "비/눈" : (PTY == 3 ? "눈" : (PTY == 4 ? "소나기" : null)))));
 
   //하늘상태 - 맑음(1), 구름많음(3), 흐림(4)
-  const skyCondition = (SKY == 1 ? "맑음" : (SKY == 3 ? "구름 많음" : (SKY == 4 ? "흐림" : null)));
+  let skyCondition;
 
-  const weatherObj = {temperature, precipitation, humidity, precipitationType, skyCondition}
+  switch (SKY) {
+    case "1":
+      skyCondition = '맑음';
+      break;
+    case "3":
+      skyCondition = '구름 많음';
+      break;
+    case "4":
+      skyCondition = '흐림';
+      break;
+    default:
+      skyCondition = '맑음';
+  }
 
+  const weatherObj = {
+    temperature,
+    precipitation,
+    humidity,
+    precipitationType,
+    skyCondition,
+  };
 
-  if(isLoading === true ||data === undefined) {
-    return <Loading/>
+  if (isLoading === true || data === undefined) {
+    return <Loading />;
   }
 
   return (
     <div className={classes.container}>
-      <Information props={weatherObj} data={data}/>
-      <Clothes props={weatherObj}  data={data}/>
+      <Information props={weatherObj} data={data} />
+      <Clothes props={weatherObj} data={data} />
     </div>
-  )
-}
+  );
+};
 
-export default Contents
+export default Contents;
