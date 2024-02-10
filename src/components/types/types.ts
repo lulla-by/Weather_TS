d4b2991790bbcdb9e66e95af5d26663119868ce2
lulla-by <thfl3363@hanmail.net>
2024-02-10 22:36:01 +0900

    types.ts 생성 - 공통 사용 타입 관리

---
+++ src/components/types/types.ts
@@ -0,0 +1,48 @@
+export interface WeatherItem {
+  LGT: string;
+  PTY: string;
+  REH: string;
+  RN1: string;
+  SKY: string;
+  T1H: string;
+  UUU: string;
+  VEC: string;
+  VVV: string;
+  WSD: string;
+}
+
+export interface ContentsWeatherItem {
+  baseDate: string;
+  baseTime: string;
+  category: string;
+  fcstDate: string;
+  fcstTime: string;
+  fcstValue: string;
+  nx: number;
+  ny: number;
+}
+
+export interface WeatherData<T> {
+  [datetime: string]: T[];
+}
+
+export interface StoreInitialType<T> {
+  current: {
+    lat: number;
+    long: number;
+  };
+  chartWeather: T;
+  region: string;
+  isLoading: boolean;
+  isLocationPermission: boolean;
+}
+
+export interface ContentsPropsType {
+  props: {
+    temperature: string;
+    precipitation: string;
+    humidity: string;
+    precipitationType: string;
+    skyCondition: string;
+  };
+}
