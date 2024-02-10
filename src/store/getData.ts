7d6cf6bcd0769a188ff917c55b2014050342db3f
lulla-by <thfl3363@hanmail.net>
2024-02-10 23:45:49 +0900

    getData 함수 타입 추가

--- src/store/getData.ts
+++ src/store/getData.ts
@@ -1,8 +1,21 @@
-export const getDataObject = (datas) => {
-  const obj = {};
+import { ContentsWeatherItem, WeatherData } from "components/types/types";
+
+
+type ProcessedWeatherData = {
+  [datetime: string]: {
+    [category: string]: {
+      [fcstDateAndTime: string]: any; // fcstValue에 대한 타입은 해당 데이터의 실제 형태에 따라 수정 필요
+      // 다른 속성이 필요하다면 추가 가능
+    };
+  };
+};
+
+export const getDataObject = (datas: WeatherData<ContentsWeatherItem>): ProcessedWeatherData => {
+
+  const obj: ProcessedWeatherData = {};
   for (const item in datas) {
     if (!obj[item]) {
-      obj[item] = [];
+      obj[item] = {};
     }
     for (let i = 0; i < datas[item].length; i++) {
       const fcstTime = datas[item][i].fcstTime;
